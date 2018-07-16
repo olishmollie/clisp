10c8395f1c8d3601a983eeaf92f383a09598b47d
ajbond <olishmollie@gmail.com>
2018-07-16 13:22:24 -0500

    Add type to ast

--- ast.c
+++ ast.c
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-ast *ast_new(token tok, int numchldrn, ast **children) {
+ast *ast_new(ast_t type, token tok, int numchldrn, ast **children) {
     ast *a = malloc(sizeof(ast));
+    a->type = type;
     a->tok = tok;
     a->numchldrn = numchldrn;
     a->children = children;
@@ -25,16 +26,21 @@ void ast_print(ast *a, int offset) {
     for (int i = 0; i < offset; i++) {
         sp[i] = ' ';
     }
-    if (a->tok.type == INT)
+    switch (a->type) {
+    case AST_NUM:
         printf("%s<type: NUM, val: '%s'>\n", sp, a->tok.val);
-    else {
-        if (a->numchldrn) {
-            printf("%s<type: EXP, val: '%s'>\n", sp, a->tok.val);
-            for (int i = 0; i < a->numchldrn; i++) {
-                ast_print(a->children[i], offset + 4);
-            }
-        } else
-            printf("%s<type: SYM, val: '%s'>\n", sp, a->tok.val);
+        break;
+    case AST_SYM:
+        printf("%s<type: SYM, val: '%s'>\n", sp, a->tok.val);
+        break;
+    case AST_EXP:
+        printf("%s<type: EXP, op: '%s'>\n", sp, a->tok.val);
+        for (int i = 0; i < a->numchldrn; i++) {
+            ast_print(a->children[i], offset + 4);
+        }
+        break;
+    case AST_ERR:
+        printf("%s<type: ERR, op: '%s'>\n", sp, a->tok.val);
     }
     free(sp);
 }
--- ast.h
+++ ast.h
@@ -3,13 +3,16 @@
 
 #include "token.h"
 
+typedef enum { AST_EXP, AST_NUM, AST_SYM, AST_ERR } ast_t;
+
 typedef struct ast {
+    ast_t type;
     token tok;
     int numchldrn;
     struct ast **children;
 } ast;
 
-ast *ast_new(token tok, int numchlrdn, ast **children);
+ast *ast_new(ast_t type, token tok, int numchlrdn, ast **children);
 void ast_delete(ast *a);
 
 void ast_print(ast *a, int offset);
