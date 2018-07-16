d64bc3c81223efe17823ef71bc3f36fcad8b3e9e
ajbond <olishmollie@gmail.com>
2018-07-16 00:49:30 -0500

    Evaluate simple expressions

--- clisp.c
+++ clisp.c
@@ -12,7 +12,7 @@
 
 #define MAXCHILDREN 10
 
-int curtok, tokenval, pos, eof = -1;
+int peektok, curtok, tokenval, pos;
 
 int nextchar(char *input)
 {
@@ -47,7 +47,7 @@ int lexsymbol(char *input)
 {
     char sym[BUFSIZE];
     int i = 0;
-    while (curchar(input) && !isdigit(curchar(input)) && !isspace(curchar(input)))
+    while (curchar(input) && !isspace(curchar(input)) && curchar(input) != ')')
     {
         sym[i++] = curchar(input);
         nextchar(input);
@@ -64,40 +64,40 @@ int lexan(char *input)
     skipspaces(input);
     int cur = curchar(input);
     if (cur == EOS)
-        curtok = END;
+        return END;
     else if (cur == '(')
     {
         nextchar(input);
         tokenval = NONE;
-        curtok = LPAR;
+        return LPAR;
     }
     else if (cur == ')')
     {
         nextchar(input);
         tokenval = NONE;
-        curtok = RPAR;
+        return RPAR;
     }
     else if (isdigit(cur))
     {
         tokenval = lexdigit(input);
-        curtok = NUM;
+        return NUM;
     }
     else
     {
         tokenval = lexsymbol(input);
-        curtok = SYM;
+        return SYM;
     }
-    return curtok;
 }
 
 int match(int type, char *input)
 {
-    if (curtok == type)
+    if (peektok == type)
     {
-        curtok = lexan(input);
+        curtok = peektok;
+        peektok = lexan(input);
         return 1;
     }
-    fprintf(stderr, "expected token type %d, got %d", type, curtok);
+    fprintf(stderr, "expected token type %d, got %d\n", type, peektok);
     return 0;
 }
 
@@ -105,21 +105,25 @@ ast *parse(char *input)
 {
     ast *child_store[MAXCHILDREN];
     int child_store_pos = 0;
-    int token = lexan(input);
-    switch (token)
+    int val;
+    switch (peektok)
     {
     case NUM:
-        return ast_new(NUM, tokenval, 0, 0);
+        val = tokenval;
+        match(NUM, input);
+        return ast_new(NUM, val, 0, 0);
     case SYM:
-        return ast_new(SYM, tokenval, 0, 0);
+        val = tokenval;
+        match(SYM, input);
+        return ast_new(SYM, val, 0, 0);
     case LPAR:
         match(LPAR, input);
-        int val = tokenval;
-        while (curtok != RPAR)
+        val = tokenval;
+        match(SYM, input);
+        while (peektok != RPAR)
         {
             ast *child = parse(input);
-            if (child)
-                child_store[child_store_pos++] = child;
+            child_store[child_store_pos++] = child;
         }
         match(RPAR, input);
         int numchldrn = child_store_pos;
@@ -134,21 +138,62 @@ ast *parse(char *input)
     return NULL;
 }
 
+long eval_op(long x, char *op, long y)
+{
+    if (strcmp("+", op) == 0)
+        return x + y;
+    if (strcmp("-", op) == 0)
+        return x - y;
+    if (strcmp("*", op) == 0)
+        return x * y;
+    if (strcmp("/", op) == 0)
+        return x / y;
+    return 0;
+}
+
+long eval(ast *root)
+{
+    if (root->type == NUM)
+    {
+        return root->val;
+    }
+    else if (root->type == EXP)
+    {
+        char *op = symtable[root->val].symbol;
+        long x = eval(root->children[0]);
+        for (int i = 1; i < root->numchldrn; i++)
+        {
+            x = eval_op(x, op, eval(root->children[i]));
+        }
+        return x;
+    }
+
+    return -1;
+}
+
 int main(void)
 {
     init();
     printf("Welcome to clisp! Use ctrl+c to exit.\n");
+
     while (1)
     {
         pos = 0;
         char *input = readline("clisp> ");
         add_history(input);
+
+        // Initialize the lexer.
+        peektok = lexan(input);
+
         ast *prog = parse(input);
         if (prog)
         {
-            ast_print(prog, 0);
+            // ast_print(prog, 0);
+            long res = eval(prog);
+            printf("%li\n", res);
             ast_delete(prog);
         }
+
         free(input);
     }
 
